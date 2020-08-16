cbf1344b594314867e1d2186d042df6caf05e1d0
anmoldeep1509 <anmoldeep1509@gmail.com>
2020-08-17 03:21:23 +0530

    removed extra

--- LearnYouNode/baby-steps.js
+++ LearnYouNode/baby-steps.js
@@ -9,14 +9,3 @@ for( let i=2; i < process.argv.length; i++){
 }
 
 console.log(result)
-
-
-// 'use strict'
-
-// let result = 0
-
-// for (let i = 2; i < process.argv.length; i++) {
-//   result += Number(process.argv[i])
-// }
-
-// console.log(result)
