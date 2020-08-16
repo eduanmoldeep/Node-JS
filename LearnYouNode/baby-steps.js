2efc482ed74e6239714e9ccc6b02bf71aed25a5e
anmoldeep1509 <anmoldeep1509@gmail.com>
2020-08-17 03:20:43 +0530

    baby Steps Solved

--- LearnYouNode/baby-steps.js
+++ LearnYouNode/baby-steps.js
@@ -1,4 +1,22 @@
 // console.log(process.argv);
 // printing cmd line arguments
+'use strict'
 
-console.log(parseInt(process.argv[2]) + parseInt(process.argv[3]));
+let result = 0
+
+for( let i=2; i < process.argv.length; i++){
+    result += Number(process.argv[i])
+}
+
+console.log(result)
+
+
+// 'use strict'
+
+// let result = 0
+
+// for (let i = 2; i < process.argv.length; i++) {
+//   result += Number(process.argv[i])
+// }
+
+// console.log(result)
